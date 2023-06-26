0499f797a79d9bb28bec603fa78e9d5d4e29cbb8
rahul paliwal r <rahulramesh2811@gmail.com>
2023-06-26 08:02:30 +0530

    Added conditions of result & adjusted moves

--- TTT.java
+++ TTT.java
@@ -1,33 +1,108 @@
 import java.util.Scanner;
 public class TTT {
     private String[][] box = {{"1", "2", "3"}, {"4", "5", "6"}, {"7","8","9"}};
-    private static String[] store = new String[6];
+    private static String[] store = new String[10];
+    int fill=1;
 
-    public void accept1(String c, int p) {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (box[i][j].equals(c) && p == 1) {
+    public void accept1(String c, int p) 
+    {
+        for (int i = 0; i < 3; i++) 
+        {
+            for (int j = 0; j < 3; j++) 
+            {
+                if (box[i][j].equals(c) && p == 1) 
+                {
                     box[i][j] = "X";
-                } else if (box[i][j].equals(c) && p == 2) {
+                } else if (box[i][j].equals(c) && p == 2) 
+                {
                     box[i][j] = "O";
                 }
             }
         }
     }
 
-    public void display() {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
+    public int display() 
+    {
+        for (int i = 0; i < 3; i++) 
+        {
+            for (int j = 0; j < 3; j++) 
+            {
                 System.out.print("|\t" + box[i][j] + "\t|");
             }
             System.out.println();
         }
+        return drawResult();
+        
+    }
+    
+    public int winResultp2() {
+        // Check rows
+        for (int i = 0; i < 3; i++) {
+            if (box[i][0].equals("O") && box[i][1].equals("O") && box[i][2].equals("O")) {
+                return 1;
+            }
+        }
+        
+        // Check columns
+        for (int j = 0; j < 3; j++) {
+            if (box[0][j].equals("O") && box[1][j].equals("O") && box[2][j].equals("O")) {
+                return 1;
+            }
+        }
+        
+        // Check diagonals
+        if (box[0][0].equals("O") && box[1][1].equals("O") && box[2][2].equals("O")) {
+            return 1;
+        }
+        if (box[0][2].equals("O") && box[1][1].equals("O") && box[2][0].equals("O")) {
+            return 1;
+        }
+        
+        return 0;
+    }
+    
+    public int winResultp1() {
+        // Check rows
+        for (int i = 0; i < 3; i++) {
+            if (box[i][0].equals("X") && box[i][1].equals("X") && box[i][2].equals("X")) {
+                return 1;
+            }
+        }
+        
+        // Check columns
+        for (int j = 0; j < 3; j++) {
+            if (box[0][j].equals("X") && box[1][j].equals("X") && box[2][j].equals("X")) {
+                return 1;
+            }
+        }
+        
+        // Check diagonals
+        if (box[0][0].equals("X") && box[1][1].equals("X") && box[2][2].equals("X")) {
+            return 1;
+        }
+        if (box[0][2].equals("X") && box[1][1].equals("X") && box[2][0].equals("X")) {
+            return 1;
+        }
+        
+        return 0;
     }
 
-    public boolean legal(String c) {
-      if (c.equals("1") || c.equals("2") || c.equals("3") || c.equals("4") || c.equals("5") || c.equals("6") || c.equals("7") || c.equals("8") || c.equals("9")) {
-        for (int j = 0; j < 6; j++) {
-            if (c.equals(store[j])) {
+    public int drawResult()
+    {
+      if(store[8]!=null)
+      return 0;
+      else
+      return 1;
+    }
+    
+    public boolean legal(String c) 
+    {
+      if (c.equals("1") || c.equals("2") || c.equals("3") || c.equals("4") || c.equals("5") || c.equals("6") || c.equals("7") || c.equals("8") || c.equals("9")) 
+      {
+        for (int j = 0; j < 9; j++) 
+        {
+            if (c.equals(store[j])) 
+            {
                 System.out.println("Illegal number, Re-enter");
                 return false;
             }
@@ -38,9 +113,12 @@ public class TTT {
       return false;
     }
 
-    public void store(String c) {
-        for (int j = 0; j < 6; j++) {
-            if (store[j] == null) {
+    public void store(String c) 
+    {
+        for (int j = 0; j < 9; j++) 
+        {
+            if (store[j] == null) 
+            {
                 store[j] = c;
                 break;
             }
@@ -52,7 +130,7 @@ public class TTT {
         TTT obj = new TTT();
         obj.display();
         
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 5; i++) {
             for (int p = 1; p < 3; p++) {
                 System.out.print("Enter the number chosen by player " + p + ": ");
                 String c = in.nextLine();
@@ -60,12 +138,21 @@ public class TTT {
                     p--;
                     continue;
                 }
-                
                 obj.store(c);
                 obj.accept1(c, p);
-                obj.display();
+                if(obj.display()==0){
+                  System.out.print("Its a DRAW!!!");
+                  System.exit(0);
+                }
+                else if(obj.winResultp1()==1){
+                  System.out.print("Player 1 WON!!!");
+                  System.exit(0);
+                }
+                else if(obj.winResultp2()==1){
+                  System.out.print("Player 2 WON!!!");
+                  System.exit(0);
+                }    
             }
         }
     }
 }
-
